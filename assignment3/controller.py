af5b10acad98cd006ef88236c4330bdcb3189cba
Ubuntu_VM <liranr23@gmail.com>
2018-05-12 10:44:54 -0700

    Try and Fail

--- assignment3/controller.py
+++ assignment3/controller.py
@@ -43,17 +43,34 @@ class Controller (EventMixin):
 
         def install_fwdrule(dpid, packet, inport, outport):
             msg = of.ofp_flow_mod()
-            # msg.idle_timeout = 10
-            # msg.hard_timeout = 30
+            msg.idle_timeout = 10
+            msg.hard_timeout = 30
+            msg.match = of.ofp_match.from_packet(packet)
+            #msg.buffer_id = None
             msg.in_port = inport
             msg.actions.append(of.ofp_action_output(port=outport))
 
             for connection in core.openflow.connections:
                 if dpid_to_str(connection.dpid) == dpid:
-                    log.info('installing on ' + dpid + 'rule from: '+ str(inport) + ' to ' + str(outport))
+                    log.info('installing on ' + dpid + ' rule from: '+ str(inport) + ' to ' + str(outport))
+                    connection.send(msg)
+
+        def send_packet(dpid, outport, data):
+            msg = of.ofp_packet_out(in_port=1, data = data)
+            msg.actions.append(of.ofp_action_output(port = outport))
+            for connection in core.openflow.connections:
+                if dpid_to_str(connection.dpid) == dpid:
+                    log.info('sending msg from: ' + dpid + ' port from: '+ str(1) + ' to ' + str(outport))
                     connection.send(msg)
 
 
+        def flood(event):
+            msg = of.ofp_packet_out()
+            msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+            msg.data = event.ofp
+            msg.in_port = event.port
+            event.connection.send(msg)
+                            
         packet = event.parsed
                 
         #To filter out ether packets ; and isinstance(packet.payload, ipv4)
@@ -61,36 +78,40 @@ class Controller (EventMixin):
         if str(packet.src) in hosts_mac:
             log.info('from host: ')
             log.info('my name is: ' +  str(packet.src))
-            log.info('my dst is: ' +  str(packet.payload.protodst))
-            log.info('dest host: ' + ip_hosts[str(packet.payload.protodst)])
-            src = host_switch[ip_hosts[str(packet.payload.protosrc)]]
-            dst = host_switch[ip_hosts[str(packet.payload.protodst)]]
+            log.info('my dst is: ' +  str(packet.next.protodst))
+            log.info('dest host: ' + ip_hosts[str(packet.next.protodst)])
+            src = host_switch[ip_hosts[str(packet.next.protosrc)]]
+            dst = host_switch[ip_hosts[str(packet.next.protodst)]]
             path = short_paths[src][dst]
             chosen_path = path[random.randint(0, len(path) - 1)]
             log.info('chosen:' + str(chosen_path))
 
             for hop in range(len(chosen_path)):
                 for link in links:
-                    if hop == 0:
-                        if link.node1 == chosen_path[hop] and link.node2 == ip_hosts[str(packet.payload.protosrc)]:
+                    if hop == len(chosen_path) - 1:
+                        if link.node1 == chosen_path[hop] and link.node2 == ip_hosts[str(packet.next.protodst)]:
                             outPort = link.port1
                             inPort = link.port2
-                    elif hop == len(chosen_path) - 1:
-                        if link.node1 == chosen_path[hop] and link.node2 == ip_hosts[str(packet.payload.protosrc)]:
-                            outPort = link.port1
-                            inPort = link.port2
+                            log.info(link)
                     elif link.node1 == chosen_path[hop] and link.node2 == chosen_path[hop + 1]:
                         outPort = link.port1
                         inPort = link.port2
+                        log.info(link)
                     elif link.node2 == chosen_path[hop] and link.node1 == chosen_path[hop + 1]:
                         outPort = link.port2
                         inPort = link.port1
+                        log.info(link)
 
                 hop_switch = filter(lambda x: x.id == chosen_path[hop], switches)[0]
                 log.info(str(hop_switch))
                 hop_dpid = hop_switch.dpidstr
                 install_fwdrule(hop_dpid, packet, inPort, outPort)
-
+            
+            # installing rule for source host to his switch
+            hop_switch = filter(lambda x: x.id == chosen_path[0], switches)[0]
+            hop_dpid = hop_switch.dpidstr
+            install_fwdrule(hop_dpid, packet, 1, 1)
+            send_packet(hop_dpid, 1, event.ofp)
 
 
 def launch():
